b4aaef34bc3e154998fde96c26f35c8d10b001c6
Mark McGranaghan <mmcgrana+github-test@gmail.com>
2012-11-06 16:39:22 -0500

    update web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello Cloud Nine World!');
 });
 
 var port = process.env.PORT || 5000;
