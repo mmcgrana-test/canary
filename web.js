59764642500f9cd6ba8e7a7d94d273621989a3df
mmcgrana-test <mmcgrana+github-test@gmail.com>
2012-11-22 15:43:09 -0800

    from my ipad

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Cloud Nine World!');
+  response.send('Hello iPad world!');
 });
 
 var port = process.env.PORT || 5000;
